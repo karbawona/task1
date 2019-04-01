e14b894b515eaf2b2aa6435e2a6db86b8958356a
karbawona <37221123+karbawona@users.noreply.github.com>
2019-04-01 20:47:47 +0200

    added something with avatar

--- app/src/main/java/com/example/homework1/ContactActivity.java
+++ app/src/main/java/com/example/homework1/ContactActivity.java
@@ -1,11 +1,15 @@
 package com.example.homework1;
 
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
 
+import java.util.Random;
 
 
 public class ContactActivity extends AppCompatActivity {
@@ -29,6 +33,7 @@ public class ContactActivity extends AppCompatActivity {
     public void onClickOk (View v) {
         Intent intent = new Intent (this, MainActivity.class);
         intent.putExtra("FULLNAME",name);
+        intent.putExtra("NUMBERAVATAR", randomNameOfAvatar());
         startActivity(intent);
 
     }
@@ -69,6 +74,18 @@ public class ContactActivity extends AppCompatActivity {
     }
 
 
+private String randomNameOfAvatar(){
 
+        String[] temp = new String[3];
+
+        temp[0] = "pobrane1";
+        temp[1] = "pobrane2";
+        temp[2] = "pobrane3";
+
+        Random rand = new Random();
+        int number = rand.nextInt(3);
+
+        return temp[number];
+    }
 
 }
--- app/src/main/java/com/example/homework1/MainActivity.java
+++ app/src/main/java/com/example/homework1/MainActivity.java
@@ -7,9 +7,12 @@
 package com.example.homework1;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -22,9 +25,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = (TextView) findViewById(R.id.textView);
+        ImageView imgView = (ImageView) findViewById(R.id.imageButton3);
 
         String fullname = getIntent().getStringExtra("FULLNAME");
+    //      String avatar = getIntent().getStringExtra("NUMBERAVATAR");
+
             textView.setText(fullname);
+    //        imgView.setImageDrawable();
 
     }
 
