29415a6c01f72764b356f765cddc3c84df6b1ad4
karbawona <37221123+karbawona@users.noreply.github.com>
2019-04-01 19:28:32 +0200

    changed textView

--- app/src/main/java/com/example/homework1/ContactActivity.java
+++ app/src/main/java/com/example/homework1/ContactActivity.java
@@ -5,13 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
-import android.widget.Toast;
+
 
 
 public class ContactActivity extends AppCompatActivity {
-
-
-String[] names = new String[5];
 
 
 
@@ -22,7 +19,22 @@ String[] names = new String[5];
 
     }
 
-    public String getNames(int i){
+    public void onClickCancel (View v) {
+        Intent i = new Intent (this, MainActivity.class);
+        startActivity(i);
+
+    }
+
+    public void onClickOk (View v) {
+        Intent i = new Intent (this, MainActivity.class);
+        String name = "aaa";
+        i.putExtra(name, "aaa");
+        startActivity(i);
+
+    }
+
+
+    public String getNames(int i) {
         String[] names = new String[5];
         names[0] = "Jan Kowalski";
         names[1] = "Anna Nowak";
@@ -33,31 +45,33 @@ String[] names = new String[5];
         return names[i];
     }
 
-    public void onClickCancel (View v) {
-        Intent i = new Intent (this, MainActivity.class);
-        startActivity(i);
 
-    }
-
-    public void onClickOk (View v) {
-        Intent i = new Intent (this, MainActivity.class);
-        startActivity(i);
-
-    }
 
     public void onRadioButtonClick(View view)
     {
-        //rzutujemy widok kontrolki, której stan został zmieniony na RadioButton, sprawdzamy czy jest wybrana i zapisujemy jej stan do zmiennej zaznaczona
-        boolean zaznaczony =((RadioButton)view).isChecked();
 
-        //sprawdzamy zmiana której kontrolki wywołała funkcję, poprzez sprawdzenie id widoku tej kontrolki
+        boolean checked =((RadioButton)view).isChecked();
+
         switch(view.getId())
         {
-            case R.id.radioButton: //gdy chodzi o pierwszy RadioButton
-                //wyswietlamy, którką informację Toast kontrolce, której stan został zmieniony
-                Toast.makeText(ContactActivity.this, "Stan RadioButton'a 1 został zmieniony na" + String.valueOf(zaznaczony),
-                        Toast.LENGTH_SHORT).show();
+            case R.id.radioButton:
+
                 break;
+
+            case R.id.radioButton2:
+                break;
+
+            case R.id.radioButton3:
+
+                break;
+            case R.id.radioButton4:
+                break;
+
+            case R.id.radioButton5:
+                break;
+
+            default:
+
 
         }
 
--- app/src/main/java/com/example/homework1/MainActivity.java
+++ app/src/main/java/com/example/homework1/MainActivity.java
@@ -10,25 +10,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // String zmienna1 = getIntent().getStringExtra("aaa");
+        TextView txt = (TextView)findViewById(R.id.textView);
+
 
     }
 
 
 
-public void onClickChangeContact (View view) {
+    public void onClickChangeContact (View view) {
 
-    Intent i = new Intent (this, ContactActivity.class);
-    startActivity(i);
-}
+        Intent i = new Intent (this, ContactActivity.class);
+        startActivity(i);
+
+    }
 
 
 public void onClickChangeSound  (View view) {
